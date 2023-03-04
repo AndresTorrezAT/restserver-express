9a3ac537b2d202ea4388ddc21ecff3e508e9affc
Andres <andres.torrez.work@gmail.com>
2023-03-04 01:55:55 -0400

    payload generado JWT

--- models/server.js
+++ models/server.js
@@ -9,7 +9,10 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+
         this.usuariosPath = '/api/usuarios';
+        this.authPath = '/api/auth';
+
 
         //Conectar a base de datos
         this.conectarDB();
@@ -40,6 +43,7 @@ class Server {
 
     routes() {
         
+        this.app.use(this.authPath, require('../routes/auth.routes'));
         this.app.use(this.usuariosPath, require('../routes/usuarios.routes'));
 
     }
