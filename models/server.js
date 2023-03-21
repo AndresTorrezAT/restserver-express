fe727b9dbb5baa048639bd3f820788495cffd0f4
[Andres] <[andres.torrez.work@gmail.com]>
2023-03-21 16:36:08 -0400

    CRUD Productos, categorias y busquedas

--- models/server.js
+++ models/server.js
@@ -12,6 +12,7 @@ class Server {
 
         this.paths = {
             auth:       '/api/auth',
+            buscar:     '/api/buscar',
             categorias: '/api/categorias',
             productos:  '/api/productos',
             usuarios:   '/api/usuarios'
@@ -47,6 +48,7 @@ class Server {
     routes() {
         
         this.app.use( this.paths.auth, require('../routes/auth.routes'));
+        this.app.use( this.paths.buscar, require('../routes/buscar.routes'));
         this.app.use( this.paths.categorias, require('../routes/categorias.routes'));
         this.app.use( this.paths.productos, require('../routes/productos.routes'));
         this.app.use( this.paths.usuarios, require('../routes/usuarios.routes'));
